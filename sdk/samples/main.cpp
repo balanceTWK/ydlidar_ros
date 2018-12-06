f953a15390533656390c47e475b31ec3593dcbe1
yangfuyuan <chushuifurong618@126.com>
2018-12-06 23:52:00 +0800

    format code style

--- sdk/samples/main.cpp
+++ sdk/samples/main.cpp
@@ -10,58 +10,60 @@ using namespace ydlidar;
 #pragma comment(lib, "ydlidar_driver.lib")
 #endif
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
 
-    std::string port;
-    std::string baudrate;
-    int baud = 115200;
-    printf("__   ______  _     ___ ____    _    ____  \n");
-    printf("\\ \\ / /  _ \\| |   |_ _|  _ \\  / \\  |  _ \\ \n");
-    printf(" \\ V /| | | | |    | || | | |/ _ \\ | |_) | \n");
-    printf("  | | | |_| | |___ | || |_| / ___ \\|  _ <  \n");
-    printf("  |_| |____/|_____|___|____/_/   \\_\\_| \\_\\ \n");
-    printf("\n");
-    fflush(stdout);
+  std::string port;
+  std::string baudrate;
+  int baud = 115200;
+  printf("__   ______  _     ___ ____    _    ____  \n");
+  printf("\\ \\ / /  _ \\| |   |_ _|  _ \\  / \\  |  _ \\ \n");
+  printf(" \\ V /| | | | |    | || | | |/ _ \\ | |_) | \n");
+  printf("  | | | |_| | |___ | || |_| / ___ \\|  _ <  \n");
+  printf("  |_| |____/|_____|___|____/_/   \\_\\_| \\_\\ \n");
+  printf("\n");
+  fflush(stdout);
 
-    std::map<std::string, std::string> lidars = YDlidarDriver::lidarPortList();
-    if(lidars.size()==1) {
-        std::map<string, string>::iterator iter = lidars.begin();
-        port = iter->second;
-    }else {
-        printf("Please enter the lidar serial port:");
-        std::cin>>port;
-        printf("Please enter the lidar serial baud rate:");
-        std::cin>>baudrate;
-        baud = atoi(baudrate.c_str());
+  std::map<std::string, std::string> lidars = YDlidarDriver::lidarPortList();
 
+  if (lidars.size() == 1) {
+    std::map<string, string>::iterator iter = lidars.begin();
+    port = iter->second;
+  } else {
+    printf("Please enter the lidar serial port:");
+    std::cin >> port;
+    printf("Please enter the lidar serial baud rate:");
+    std::cin >> baudrate;
+    baud = atoi(baudrate.c_str());
+
+  }
+
+
+
+
+  ydlidar::init(argc, argv);
+  CYdLidar laser;
+  laser.setSerialPort(port);
+  laser.setSerialBaudrate(baud);
+  laser.setFixedResolution(false);
+  laser.setReversion(false);
+  laser.setAutoReconnect(true);
+  laser.initialize();
+
+  while (ydlidar::ok()) {
+    bool hardError;
+    LaserScan scan;
+
+    if (laser.doProcessSimple(scan, hardError)) {
+      fprintf(stdout, "Scan received: %u ranges\n", (unsigned int)scan.ranges.size());
+      fflush(stdout);
     }
+  }
 
 
+  laser.turnOff();
+  laser.disconnecting();
 
-
-    ydlidar::init(argc, argv);
-    CYdLidar laser;
-    laser.setSerialPort(port);
-    laser.setSerialBaudrate(baud);
-    laser.setFixedResolution(false);
-    laser.setReversion(false);
-    laser.setAutoReconnect(true);
-    laser.initialize();
-    while(ydlidar::ok()){
-		bool hardError;
-		LaserScan scan;
-		if(laser.doProcessSimple(scan, hardError )){
-            fprintf(stdout,"Scan received: %u ranges\n",(unsigned int)scan.ranges.size());
-            fflush(stdout);
-		}
-	}
-
-
-    laser.turnOff();
-    laser.disconnecting();
-
-    return 0;
+  return 0;
 
 
 }
--- src/ydlidar_node.cpp
+++ src/ydlidar_node.cpp
@@ -1,10 +1,10 @@
 /*
  *  YDLIDAR SYSTEM
- *  YDLIDAR ROS Node Client 
+ *  YDLIDAR ROS Node Client
  *
  *  Copyright 2015 - 2018 EAI TEAM
  *  http://www.ydlidar.com
- * 
+ *
  */
 
 #include "ros/ros.h"
@@ -21,108 +21,114 @@ using namespace ydlidar;
 
 
 std::vector<float> split(const std::string &s, char delim) {
-    std::vector<float> elems;
-    std::stringstream ss(s);
-    std::string number;
-    while(std::getline(ss, number, delim)) {
-        elems.push_back(atof(number.c_str()));
-    }
-    return elems;
+  std::vector<float> elems;
+  std::stringstream ss(s);
+  std::string number;
+
+  while (std::getline(ss, number, delim)) {
+    elems.push_back(atof(number.c_str()));
+  }
+
+  return elems;
 }
 
 
-int main(int argc, char * argv[]) {
-    ros::init(argc, argv, "ydlidar_node"); 
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "ydlidar_node");
 
-    std::string port;
-    int baudrate=115200;
-    std::string frame_id;
-    bool angle_fixed,reversion, resolution_fixed;
-    bool auto_reconnect;
-    double angle_max,angle_min;
-    result_t op_result;
-    std::string list;
-    std::vector<float> ignore_array;  
-    double max_range, min_range;
+  std::string port;
+  int baudrate = 115200;
+  std::string frame_id;
+  bool angle_fixed, reversion, resolution_fixed;
+  bool auto_reconnect;
+  double angle_max, angle_min;
+  result_t op_result;
+  std::string list;
+  std::vector<float> ignore_array;
+  double max_range, min_range;
 
-    ros::NodeHandle nh;
-    ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("scan", 1000);
-    ros::NodeHandle nh_private("~");
-    nh_private.param<std::string>("port", port, "/dev/ydlidar"); 
-    nh_private.param<int>("baudrate", baudrate, 115200); 
-    nh_private.param<std::string>("frame_id", frame_id, "laser_frame");
-    nh_private.param<bool>("angle_fixed", angle_fixed, "true");
-    nh_private.param<bool>("resolution_fixed", resolution_fixed, "true");
-    nh_private.param<bool>("auto_reconnect", auto_reconnect, "true");
-    nh_private.param<bool>("reversion", reversion, "false");
-    nh_private.param<double>("angle_max", angle_max , 180);
-    nh_private.param<double>("angle_min", angle_min , -180);
-    nh_private.param<double>("range_max", max_range , 16.0);
-    nh_private.param<double>("range_min", min_range , 0.08);
-    nh_private.param<std::string>("ignore_array",list,"");
+  ros::NodeHandle nh;
+  ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("scan", 1000);
+  ros::NodeHandle nh_private("~");
+  nh_private.param<std::string>("port", port, "/dev/ydlidar");
+  nh_private.param<int>("baudrate", baudrate, 115200);
+  nh_private.param<std::string>("frame_id", frame_id, "laser_frame");
+  nh_private.param<bool>("angle_fixed", angle_fixed, "true");
+  nh_private.param<bool>("resolution_fixed", resolution_fixed, "true");
+  nh_private.param<bool>("auto_reconnect", auto_reconnect, "true");
+  nh_private.param<bool>("reversion", reversion, "false");
+  nh_private.param<double>("angle_max", angle_max, 180);
+  nh_private.param<double>("angle_min", angle_min, -180);
+  nh_private.param<double>("range_max", max_range, 16.0);
+  nh_private.param<double>("range_min", min_range, 0.08);
+  nh_private.param<std::string>("ignore_array", list, "");
 
-    ignore_array = split(list ,',');
-    if(ignore_array.size()%2){
-        ROS_ERROR_STREAM("ignore array is odd need be even");
+  ignore_array = split(list, ',');
+
+  if (ignore_array.size() % 2) {
+    ROS_ERROR_STREAM("ignore array is odd need be even");
+  }
+
+  for (uint16_t i = 0 ; i < ignore_array.size(); i++) {
+    if (ignore_array[i] < -180 && ignore_array[i] > 180) {
+      ROS_ERROR_STREAM("ignore array should be between -180 and 180");
+    }
+  }
+
+  ROS_INFO("[YDLIDAR INFO]: ROS SDK VERSION: %s", ROSVerision);
+  CYdLidar laser;
+
+  if (angle_max < angle_min) {
+    double temp = angle_max;
+    angle_max = angle_min;
+    angle_min = temp;
+  }
+
+  laser.setSerialPort(port);
+  laser.setSerialBaudrate(baudrate);
+  laser.setMaxRange(max_range);
+  laser.setMinRange(min_range);
+  laser.setMaxAngle(angle_max);
+  laser.setMinAngle(angle_min);
+  laser.setReversion(reversion);
+  laser.setFixedResolution(resolution_fixed);
+  laser.setAutoReconnect(auto_reconnect);
+  laser.setReversion(reversion);
+  laser.setIgnoreArray(ignore_array);
+  laser.initialize();
+
+  ros::Rate rate(30);
+
+  while (ros::ok()) {
+    bool hardError;
+    LaserScan scan;//原始激光数据
+
+    if (laser.doProcessSimple(scan, hardError)) {
+      sensor_msgs::LaserScan scan_msg;
+      ros::Time start_scan_time;
+      start_scan_time.sec = scan.system_time_stamp / 1000000000ul;
+      start_scan_time.nsec = scan.system_time_stamp % 1000000000ul;
+      scan_msg.header.stamp = start_scan_time;
+      scan_msg.header.frame_id = frame_id;
+      scan_msg.angle_min = scan.config.min_angle;
+      scan_msg.angle_max = scan.config.max_angle;
+      scan_msg.angle_increment = scan.config.ang_increment;
+      scan_msg.scan_time = scan.config.scan_time;
+      scan_msg.time_increment = scan.config.time_increment;
+      scan_msg.range_min = scan.config.min_range;
+      scan_msg.range_max = scan.config.max_range;
+
+      scan_msg.ranges = scan.ranges;
+      scan_msg.intensities =  scan.intensities;
+      scan_pub.publish(scan_msg);
     }
 
-    for(uint16_t i =0 ; i < ignore_array.size();i++){
-        if(ignore_array[i] < -180 && ignore_array[i] > 180){
-            ROS_ERROR_STREAM("ignore array should be between -180 and 180");
-        }
-    }
+    rate.sleep();
+    ros::spinOnce();
+  }
 
-    ROS_INFO("[YDLIDAR INFO]: ROS SDK VERSION: %s", ROSVerision);
-    CYdLidar laser;
-    if(angle_max < angle_min){
-        double temp = angle_max;
-        angle_max = angle_min;
-        angle_min = temp;
-    }
-
-    laser.setSerialPort(port);
-    laser.setSerialBaudrate(baudrate);
-    laser.setMaxRange(max_range);
-    laser.setMinRange(min_range);
-    laser.setMaxAngle(angle_max);
-    laser.setMinAngle(angle_min);
-    laser.setReversion(reversion);
-    laser.setFixedResolution(resolution_fixed);
-    laser.setAutoReconnect(auto_reconnect);
-    laser.setReversion(reversion);
-    laser.setIgnoreArray(ignore_array);
-    laser.initialize();
-
-    ros::Rate rate(30);
-
-    while (ros::ok()) {
-        bool hardError;
-        LaserScan scan;//原始激光数据
-        if(laser.doProcessSimple(scan, hardError )){
-            sensor_msgs::LaserScan scan_msg;
-            ros::Time start_scan_time;
-            start_scan_time.sec = scan.system_time_stamp/1000000000ul;
-            start_scan_time.nsec = scan.system_time_stamp%1000000000ul;
-            scan_msg.header.stamp = start_scan_time;
-            scan_msg.header.frame_id = frame_id;
-            scan_msg.angle_min = scan.config.min_angle;
-            scan_msg.angle_max = scan.config.max_angle;
-            scan_msg.angle_increment = scan.config.ang_increment;
-            scan_msg.scan_time = scan.config.scan_time;
-            scan_msg.time_increment = scan.config.time_increment;
-            scan_msg.range_min = scan.config.min_range;
-            scan_msg.range_max = scan.config.max_range;
-            
-            scan_msg.ranges = scan.ranges;
-            scan_msg.intensities =  scan.intensities;
-            scan_pub.publish(scan_msg);
-        }  
-        rate.sleep();
-        ros::spinOnce();
-    }
-
-    laser.turnOff();
-    printf("[YDLIDAR INFO] Now YDLIDAR is stopping .......\n");
-    laser.disconnecting();
-    return 0;
+  laser.turnOff();
+  printf("[YDLIDAR INFO] Now YDLIDAR is stopping .......\n");
+  laser.disconnecting();
+  return 0;
 }
